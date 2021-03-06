2e4af8d15fcb6c020400ee6af2a32a72fefd72dd
JibrilFl <jibrilfl@gmail.com>
2021-03-06 21:57:25 +0500

    finisch

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,17 +2,23 @@ window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
           menuItem = document.querySelectorAll('.menu_item'),
           hamburger = document.querySelector('.hamburger'),
-          logo = document.querySelector('.promo__logo_img');
+          logo = document.querySelectorAll('.promo__logo_img');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('menu_active');
+        logo.forEach(item => {
+            item.classList.toggle('promo__logo_active');
+        });
     });
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
             menu.classList.toggle('menu_active');
+            logo.forEach(i => {
+                i.classList.toggle('promo__logo_active');
+            });
         });
     });
 });
--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,14 @@ window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
           menuItem = document.querySelectorAll('.menu_item'),
           hamburger = document.querySelector('.hamburger'),
-          logo = document.querySelector('.promo__logo_img');
+          logo = document.querySelectorAll('.promo__logo_img');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('menu_active');
+        logo.forEach(item => {
+            item.classList.toggle('promo__logo_active');
+        });
     });
 
     menuItem.forEach(item => {
